21a089da30c08dd775cb532aa452ab0a83b7b56b
captain-c00keys <gpieterson.wsu@hotmail.com>
2019-07-15 19:53:36 -0700

    minor changes

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import ReactMapGL from 'react-map-gl';
+import MapGL, { Marker } from 'react-map-gl';
+import Pin from './Pin'
 
 export default class Map extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            api_url: 'https://data.edmonton.ca/resource/87ck-293k.json',
             viewport: {
                 width: 1100,
                 height: 600,
@@ -13,16 +15,40 @@ export default class Map extends Component {
                 latitude:47.608013, 
                 longitude: -122.335167,
             },
+            coords: [
+                { latitude: 47.609657, longitude: -122.342148 },
+                { latitude: 38.8894628780114, longitude: -77.1276390552521 },
+                { latitude: 47.6236555, longitude: -122.3602975 },
+            ],
+            data: null,
+        };
+    }
+
+    componentDidMount() {
+        const { data, api_url } = this.state;
+
+        if(!data) {
+            fetch(api_url, { method: 'GET' })
+            .then(response => response.json())
+            .then(response => this.setState({ data: response }));
         }
     }
 
     render() {
+    const { coords, data } = this.state;
+
         return (
-            <ReactMapGL
+            <MapGL
             mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         {...this.state.viewport}
-        onViewportChange={(viewport) => this.setState({viewport})}
-      />
+        onViewportChange={viewport => this.setState({ viewport })}>
+
+        {data && data.map((coord, i) => (
+            <Marker key={`Marker-${i * (Math.random() * 200 + 1)}`} latitude={coord.location.latitude} longitude={coord.location.longitude}>
+                <Pin />
+            </Marker>
+        ))}
+            </MapGL>
         );
     }
 }
